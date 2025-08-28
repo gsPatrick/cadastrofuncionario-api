d3b40e86cbceedd538d82b18244a05a5a0b06046
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-08-28 06:13:20 -0300

    finalizado

--- src/app.js
+++ src/app.js
@@ -31,8 +31,8 @@ app.use(cors({
 // CORREÇÃO: Aumentando o limite do corpo da requisição
 // Adicione as opções { limit: '50mb' } aqui.
 // ======================================================================
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '1gb' }));
+app.use(express.urlencoded({ limit: '1gb', extended: true }));
 
 const uploadDir = 'uploads';
 if (!fs.existsSync(uploadDir)){
